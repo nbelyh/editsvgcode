dddd55308fb1abf35cee7c632c62eb3fb4de204d
Nikolay Belykh <nbelyh@gmail.com>
2026-03-19 19:31:57 +0500

    remove visualizer file

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({ filename: 'stats.html', gzipSize: true }),
-  ],
+  plugins: [react()],
   build: {
     outDir: 'dist',
   },
